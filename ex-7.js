74a416b99425a40b6fbe920154de01dd27fe776a
Wascharapong Phummon <rickyharvey.w@gmail.com>
2024-05-13 14:43:19 +0700

    feat: Arrays Assignment

--- ex-7.js
+++ ex-7.js
@@ -42,7 +42,17 @@ let orders = [
 ];
 
 // Start coding here
-let isArray;
-let creditCardTypeOfBlindermann;
-let productQuantityOfJoannet;
-let totalPurchaseOfDary;
+let isArray = Array.isArray(orders);
+console.log(isArray);
+
+let creditCardTypeOfBlindermann = orders[2].creditCardType;
+
+orders[2].creditCardType = "visa";
+
+let productQuantityOfJoannet = orders[3].productQuantity;
+
+let totalPurchaseOfDary = orders[1].productQuantity * orders[1].productPrice;
+
+orders.shift();
+console.log(orders);
+
